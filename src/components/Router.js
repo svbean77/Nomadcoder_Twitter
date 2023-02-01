ccc480f27d0fac37286c09bfa44f4144337ad75f
Eunbeen Oh <svbean77@naver.com>
2023-02-01 20:42:17 +0900

    learn nweet

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,18 +1,17 @@
-import React, { useState } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 import Navigation from "./Navigation";
 import Profile from "../routes/Profile";
 
-const AppRouter = ({ isLoggedIn }) => {
+const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
     <Router>
       {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
-            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/" element={<Home userObj={userObj} />}></Route>
             <Route exact path="/profile" element={<Profile />}></Route>
           </>
         ) : (
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,80 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useEffect, useState } from "react";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import Nweet from "components/Nweet";
 
-const Home = () => <span>Home</span>;
+const Home = ({ userObj }) => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  /*
+  //forEach: 이건 좀 예전 방식임 -> onSnapshot을 사용
+  const getNweets = async () => {
+    const dbNweets = await getDocs(query(collection(dbService, "nweets")));
+    dbNweets.forEach((doc) => {
+      const nweetObject = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+  */
+  useEffect(() => {
+    //getNweets();
+    onSnapshot(
+      query(collection(dbService, "nweets"), orderBy("createdAt", "desc")),
+      (snapshot) => {
+        const nweetArray = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setNweets(nweetArray);
+      }
+    );
+  }, []);
+
+  const onChange = (event) => {
+    setNweet(event.target.value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await addDoc(collection(dbService, "nweets"), {
+      text: nweet,
+      createdAt: serverTimestamp(),
+      creatorId: userObj.uid,
+    });
+    setNweet("");
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input onClick={onSubmit} type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets.map((nweet) => (
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Home;
